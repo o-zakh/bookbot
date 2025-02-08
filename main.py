1efb3362d165425c5050221910df5bca7e94175b
oleg <o.zakharov@nexters.com>
2025-02-08 23:30:34 +0400

    Разделил код по функциям для удобства чтения

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    print(f"В книге {count_words(read_text("books/frankenstein.txt"))} слов")
+
+# Функция открытия книги и возврата текста:
+def read_text(book_path):
+    print("Открываю книгу...")
+    with open(book_path) as f:
         file_contents = f.read()
-        print(file_contents)
+        print("Возвращаю текст книги...")
+        return file_contents
+
+# Функция подсчета слов в книге:
+def count_words(file_contents):
+    print("Начинаю подсчет слов")
+    word_num = len(file_contents.split())
+    return word_num
 
 main()
