45e335cbc86f41a1616ed990011e85c8e5a7ff71
oleg <o.zakharov@nexters.com>
2025-02-09 00:23:38 +0400

    Добавлена функция подсчета всех букв в тексте

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def main():
-    print(f"В книге {count_words(read_text("books/frankenstein.txt"))} слов")
+    lowered_letters_text = lower_letters(read_text("books/frankenstein.txt"))
+    char_num(lowered_letters_text)
 
 # Функция открытия книги и возврата текста:
 def read_text(book_path):
@@ -14,5 +15,20 @@ def count_words(file_contents):
     print("Начинаю подсчет слов")
     word_num = len(file_contents.split())
     return word_num
+
+# Функция уменьшения регистра букв:
+def lower_letters(file_contents):
+    lowered_string = file_contents.lower()
+    return lowered_string
+
+#Функция подсчета каждого символа в тексте:
+def char_num(text_to_process):
+    output_dict = {}
+    for char in text_to_process:
+        if char in output_dict:
+            output_dict[char] += 1 
+        if char not in output_dict:
+            output_dict[char] = 1
+    print(output_dict)
 
 main()
